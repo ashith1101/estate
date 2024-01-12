340a9107f699c11bc0740eefebbb8994529e5e20
Ashith Shetty <ashithdshetty1101@gmail.com>
2024-01-12 11:55:08 +0530

    user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,32 @@
+import { errorHandler } from "../utils/error.js"
+import bcrypt from 'bcryptjs';
+import User from "../models/user.model.js";
+
+
 export const test=(req,res)=>{
     res.json({message:"Hello World"})
+}
+
+export const updateUser=async(req,res,next)=>{
+    if(req.user.id!=req.params.id) return next(errorHandler(403,'You can only update you own account'))
+    try{
+        if(req.body.password){
+            req.body.password=bcrypt.hashSync(req.body.password, 10);
+        }
+        const updateUser= await User.findByIdAndUpdate(req.params.id, {
+            $set:{
+                name:req.body.name,
+                email:req.body.email,
+                password:req.body.password,
+                avatar:req.body.avatar,
+            }
+        },
+        {new:true}
+        );
+        const {password, ...userInfo}=updateUser._doc;
+        res.status(200).json(userInfo);
+    }
+    catch(error){
+        next(error)
+    }
 }
