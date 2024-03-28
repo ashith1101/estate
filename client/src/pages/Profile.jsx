23cb00b4efca15c31d5b1e28c1e29cde9d690e90
Ashith Shetty <ashithdshetty1101@gmail.com>
2024-03-28 12:56:56 +0530

    complete update listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -30,6 +30,12 @@ export default function Profile() {
   const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
+  // firebase storage
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+
   useEffect(() => {
     if (file) {
       handleFileUpload(file);
@@ -188,7 +194,7 @@ export default function Profile() {
           type='text'
           placeholder='name'
           defaultValue={currentUser.name}
-          id='username'
+          id='name'
           className='border p-3 rounded-lg'
           onChange={handleChange}
         />
